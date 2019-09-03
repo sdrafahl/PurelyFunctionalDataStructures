85233549e126df4f69d120d53581700403d4909c
Shane Drafahl <sdrafahl@pop-os.localdomain>
2019-09-02 23:10:37 -0500

    Adding skeleton code for linked list, needs to be tested. Saving for the night.

--- purelyfunctionaldatastructures/src/main/scala/list/List.scala
+++ purelyfunctionaldatastructures/src/main/scala/list/List.scala
@@ -1,12 +1,38 @@
 package List
 
-case class Node(item: Ordered, child: Node)
+case class Node(item: Int, child: Option[Node])
 
 abstract class FunctionalList {
-  def append(value: Ordered): FunctionalList
+  val head : Option[Node]
+  val tail : Option[Node]
+  def append(value: Int): FunctionalList
 }
 
 object FunctionalList {
-  def apply() = new FunctionalList
+  def apply(givenHead: Option[Node] = None, givenTail: Option[Node] = None) = FunctionalList {
+    val head = givenHead
+    val tail = givenTail
+    def append(value: Int): FunctionalList = {
+      def appendNode(value : Int, node: Node): Node = node match {
+        case Node(v, None) => Node(v, Node(value, None))
+        case Node(v, c) => Node(v, appendNode(value, c))
+      }
+      def getTail(node: Node): Node = node match {
+        case Node(_, None) => node
+        case Node(_, c) => getTail(c)
+      }
+      head match {
+        case None => {
+          val newNode = Some(Node(value, None))
+          FunctionalList(newNode, newNode)
+        }
+        case _ => {
+          val newLinkedNodes = appendNode(valpue, head)
+          val tail = getTail(newLinkedNodes)
+          FunctionalList(head, newLinkedNodes)
+        }
+      }
+    }
+  }
   implicit lazy val functionalList = FunctionalList() 
 }
