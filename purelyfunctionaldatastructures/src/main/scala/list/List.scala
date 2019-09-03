9ad290e2c197526d404cae1eaa58bb1cb93c65da
Shane Drafahl <sdrafahl@pop-os.localdomain>
2019-09-02 23:10:37 -0500

    Fixing functional list and added method to print list.

--- purelyfunctionaldatastructures/src/main/scala/list/List.scala
+++ purelyfunctionaldatastructures/src/main/scala/list/List.scala
@@ -1,12 +1,50 @@
 package List
 
-case class Node(item: Ordered, child: Node)
+case class Node(item: Int, child: Option[Node])
 
 abstract class FunctionalList {
-  def append(value: Ordered): FunctionalList
+  val head : Option[Node]
+  val tail : Option[Node]
+  def append(value: Int): FunctionalList
 }
 
 object FunctionalList {
-  def apply() = new FunctionalList
+  def apply(givenHead: Option[Node] = None, givenTail: Option[Node] = None): FunctionalList = new FunctionalList {
+    val head = givenHead
+    val tail = givenTail
+    def append(value: Int): FunctionalList = {
+      def appendNode(value : Int, node: Node): Node = node match {
+        case Node(v, None) => Node(v, Some(Node(value, None)))
+        case Node(v, Some(c)) => Node(v, Some(appendNode(value, c)))
+      }
+      def getTail(node: Node): Node = node match {
+        case Node(_, None) => node
+        case Node(_, Some(c)) => getTail(c)
+      }
+      head match {
+        case None => {
+          val newNode = Some(Node(value, None))
+          FunctionalList(newNode, newNode)
+        }
+        case Some(n) => {
+          val newLinkedNodes = appendNode(value, n)
+          val tail = getTail(newLinkedNodes)
+          FunctionalList(Some(newLinkedNodes), Some(tail))
+        }
+      }
+    }
+    override def toString: String = {
+      def nodesToString(node: Node): String = {
+        node match {
+          case Node(v, None) => "" + v
+          case Node(v, Some(c)) => v + "|" + nodesToString(c)
+        }
+      }
+      head match {
+        case None => "|"
+        case Some(n) => nodesToString(n)
+      }
+    }
+  }
   implicit lazy val functionalList = FunctionalList() 
 }
