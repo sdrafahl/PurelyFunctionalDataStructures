1d2d67361df638ac587c4252ce62358a495271cb
Shane Drafahl <sdrafahl@pop-os.localdomain>
2019-09-08 19:23:56 -0500

    Adding interface information for the queue.

--- purelyfunctionaldatastructures/src/main/scala/queue/AmoritizedQueue.scala
+++ purelyfunctionaldatastructures/src/main/scala/queue/AmoritizedQueue.scala
@@ -1,5 +1,7 @@
 package AmoritizedQueue
 
-abstract class AmoritizedQueue {
-
+abstract class AmoritizedQueue[A] {
+  def head : A
+  def tail : AmoritizedQueue[A]
+  def snoc : AmoritizedQueue[A] 
 }
