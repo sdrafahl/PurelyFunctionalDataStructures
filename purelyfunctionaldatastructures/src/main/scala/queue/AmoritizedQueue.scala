8ecac4c76764f8023b28c020bf12a50313c79f9d
Shane Drafahl <sdrafahl@pop-os.localdomain>
2019-09-09 00:01:02 -0500

    Saving work for the night.

--- purelyfunctionaldatastructures/src/main/scala/queue/AmoritizedQueue.scala
+++ purelyfunctionaldatastructures/src/main/scala/queue/AmoritizedQueue.scala
@@ -1,7 +1,42 @@
 package AmoritizedQueue
 
+import Stream._
+
 abstract class AmoritizedQueue[A] {
-  def head : A
+  def head : Option[A]
   def tail : AmoritizedQueue[A]
-  def snoc : AmoritizedQueue[A] 
+  def snoc(item: A) : AmoritizedQueue[A] 
+}
+
+object AmoritizedQueue {
+  implicit def apply[A]: AmoritizedQueue[A] = createQueue[A](None, None, 0, 0)
+
+  private def createQueue[A](frontStream: Option[Stream[A, Stream[_, _]]], rearStream: Option[Stream[A, Stream[_, _]]], frontLength: Int, secondLength: Int) : AmoritizedQueue[A] = new AmoritizedQueue[A] {
+    def head = frontStream match {
+      case None => None
+      case Some(stream) => stream.f  
+    }
+
+    def tail = frontStream match {
+      case None => AmoritizedQueue[A]
+    }
+
+    def snoc(item: A) = frontLength == secondLength || reverseLength == 0 match {
+      case true =>
+      case false => 
+    }
+
+    lazy val frontLength = frontLength
+
+    lazy val reverseLength = secondLength
+
+    private def rotate =
+
+    private def reverseStream(inputStream: Stream[Option[A], Option[Stream[_, _]]], result: Stream[Option[A], Option[Stream[_, _]]]): Stream[A, A] = {
+      inputStream.s match {
+        case Some(_) => reverseStream(inputStream.s, Stream[inputStream.f, None])
+        case None => Stream(inputStream.f, result)
+      }
+    }
+  }
 }
--- purelyfunctionaldatastructures/src/test/scala/AmoritizedQueue/AmoritizedQueue.spec.scala
+++ purelyfunctionaldatastructures/src/test/scala/AmoritizedQueue/AmoritizedQueue.spec.scala
@@ -4,6 +4,34 @@ import org.scalatest._
 
 class AmoritizedQueueTest extends FunSpec with Matchers {
   describe("AmoritizedQueue") {
+    val testQueue = AmoritizedQueue[String]
+    describe("head") {
+      describe("Empty queue") {
+        it("Should return the default value when the queue is empty") {
+          val testQueue = AmoritizedQueue[String]
+          assert(testQueue.head.isEmpty)
+        }
+      }
+    }
 
+    describe("tail") {
+      describe("Empty queue"){
+        it("Should return an empty queue") {
+          val emptyQueue = testQueue.tail
+          assert(emptyQueue.head.isEmpty)
+        }
+      }
+
+      describe("Non empty queue") {
+      }
+    }
+
+    describe("snoc") {
+      it("Should return a queue after adding an item") {
+        val testString = "testString"
+        val queueWithOneItem = testQueue.snoc(testString)
+        assert(queueWithOneItem.head.get == testString)
+      }
+    }
   }
 }
